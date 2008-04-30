7e0f1113109441c2cbedf797f24ee321aeb4fe25
Eric Lindvall <eric@5stops.com>
2008-04-29 23:02:54 -0700

    Add "total" commands to mysql statistics.

--- mysql_query_statistics.rb
+++ mysql_query_statistics.rb
@@ -20,17 +20,21 @@ class MysqlQueryStatistics < Scout::Plugin
     result = mysql.query('SHOW /*!50002 GLOBAL */ STATUS')
 
     rows = []
+    total = 0
     result.each do |row| 
       rows << row if ENTRIES.include?(row.first)
+
+      total += row.last.to_i if row.first[0..3] == 'Com_'
     end
     result.free
-
 
     report = {}
     rows.each do |row|
       name = row.first[/_(.*)$/, 1]
       report[name] = calculate_counter(now, name, row.last.to_i)
     end
+
+    report['total'] = calculate_counter(now, 'total', total)
 
     { :report => report, :memory => @memory }
   end
