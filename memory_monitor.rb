9bfda0a50d1d79ff9d7ae90c183184d3815ebd27
Eric Lindvall <eric@5stops.com>
2009-09-09 00:55:33 -0700

    Providing paging statistics for memory.

--- memory_monitor.rb
+++ memory_monitor.rb
@@ -2,27 +2,25 @@
 # Created by Eric Lindvall <eric@5stops.com>
 #
 
+require File.dirname(__FILE__) + '/scout_counter'
+
 class MemoryMonitor < Scout::Plugin
+  include ScoutCounter
+
   def build_report
     unless File.exists?('/proc/meminfo')
       return error(%Q(Unable to find /proc/meminfo. Please ensure your operationg system supports procfs:
                        http://en.wikipedia.org/wiki/Procfs))
     end
 
-    mem_info = {}
-    IO.foreach('/proc/meminfo') do |line|
-      _, key, value = *line.match(/^(\w+):\s+(\d+)\s/)
-      mem_info[key] = value.to_i
-    end
-
     mem_total = mem_info['MemTotal'] / 1024
-    mem_free = (mem_info['MemFree'] + mem_info['Buffers'] + mem_info['Cached']) / 1024
-    mem_used = mem_total - mem_free
+    mem_free  = (mem_info['MemFree'] + mem_info['Buffers'] + mem_info['Cached']) / 1024
+    mem_used  = mem_total - mem_free
     mem_percent_used = (mem_used / mem_total.to_f * 100).to_i
 
     swap_total = mem_info['SwapTotal'] / 1024
-    swap_free = mem_info['SwapFree'] / 1024
-    swap_used = swap_total - swap_free
+    swap_free  = mem_info['SwapFree'] / 1024
+    swap_used  = swap_total - swap_free
     swap_percent_used = (swap_used / swap_total.to_f * 100).to_i
 
     report('Memory Total'  => mem_total)
@@ -32,8 +30,45 @@ class MemoryMonitor < Scout::Plugin
     report('Swap Total'    => swap_total)
     report('Swap Used'     => swap_used)
     report('% Swap Used'   => swap_percent_used)
+
+    if vmstat?
+      counter('Page-ins'    => vmstat['pgpgin'])
+      counter('Page-outs'   => vmstat['pgpgout'])
+      counter('Page Faults' => vmstat['pgfault'])
+    end
   rescue Exception => e
     error("An error occurred profiling the memory:\n\n#{e.class}: #{e.message}\n\t#{e.backtrace.join("\n\t")}")
   end
+
+  def mem_info?
+    File.exists?('/proc/meminfo')
+  end
+
+  def mem_info
+    @mem_info ||= begin
+      hash = {}
+      IO.foreach('/proc/meminfo') do |line|
+        _, key, value = *line.match(/^(\w+):\s+(\d+)/)
+        hash[key] = value.to_i
+      end
+      hash
+    end
+  end
+
+  def vmstat?
+    File.exists?('/proc/vmstat')
+  end
+
+  def vmstat
+    @vmstat ||= begin
+      hash = {}
+      IO.foreach('/proc/vmstat') do |line|
+        _, key, value = *line.match(/^(\w+)\s+(\d+)/)
+        hash[key] = value.to_i
+      end
+      hash
+    end
+  end
+
 end
 
--- mysql_query_statistics.rb
+++ mysql_query_statistics.rb
@@ -3,8 +3,12 @@
 #
 
 require 'set'
+require File.dirname(__FILE__) + '/scout_counter'
+
 
 class MysqlQueryStatistics < Scout::Plugin
+  include ScoutCounter
+
   ENTRIES = %w(Com_insert Com_select Com_update Com_delete).to_set
 
   def build_report
@@ -17,7 +21,6 @@ class MysqlQueryStatistics < Scout::Plugin
     user = @options['user'] || 'root'
     password, host, port, socket = @options.values_at(*%w(password host port socket)).collect { |v| v == '' ? nil : v }
 
-    now    = Time.now
     mysql  = Mysql.connect(host, user, password, nil, port, socket)
     result = mysql.query('SHOW /*!50002 GLOBAL */ STATUS')
 
@@ -32,37 +35,12 @@ class MysqlQueryStatistics < Scout::Plugin
 
     rows.each do |row|
       name = row.first[/_(.*)$/, 1]
-      report(name => calculate_counter(now, name, row.last.to_i))
+      counter(name => row.last.to_i)
     end
 
-    report('total' => calculate_counter(now, 'total', total))
+    counter('total' => total)
   rescue Exception => e
     error("An error occurred profiling mysql:\n\n#{e.class}: #{e.message}\n\t#{e.backtrace.join("\n\t")}")
-  end
-
-  private
-  def calculate_counter(current_time, name, value)
-    result = nil
-
-    if data = memory(name) && data.is_a?(Hash)
-      last_time, last_value = data.values_at(:time, :value)
-
-      # We won't log it if the value has wrapped
-      if value >= last_value
-        elapsed_seconds = last_time - current_time
-        elapsed_seconds = 1 if elapsed_seconds < 1
-
-        result = value - last_value
-
-        if @options['calculate_per_second']
-          result = result / elapsed_seconds.to_f
-        end
-      end
-    end
-
-    remember(name => { :time => current_time, :value => value })
-
-    result
   end
 end
 
