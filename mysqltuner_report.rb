1347481f0086b8231a85cc600705c9c2aa399c66
Eric Lindvall <eric@sevenscale.com>
2009-12-10 01:11:04 -0800

    Add plugin for mysqltuner.pl

---
+++ mysqltuner_report.rb
@@ -0,0 +1,17 @@
+# 
+# Simple plugin to generate mysqltuner.pl summary
+#
+# Created by Eric Lindvall <eric@sevenscale.com>
+#
+
+class MysqltunerReport < Scout::Plugin
+  def build_report
+    cmd = "curl -s http://mysqltuner.pl/mysqltuner.pl | perl - --nocolor"
+    data = `#{cmd} 2>&1`
+    unless $?.success?
+      return error "Could not get data from command: #{cmd}", "Error: #{data}"
+    end   
+
+    summary :command => 'mysqltuner.pl', :output => "<pre>#{data}</pre>"
+  end
+end
